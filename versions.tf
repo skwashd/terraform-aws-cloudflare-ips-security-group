6a22365ba4801c20a9d6c323b2a435172ce55921
Dave Hall <skwashd@gmail.com>
2025-01-04 13:33:46 +1100

    Improve version constraints

--- versions.tf
+++ versions.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.0.0, < 2.0.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.13.0"
+      version = ">= 5.0.0, < 6.0.0"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 4.0.0"
+      version = ">= 4.0.0, < 5.0.0"
     }
   }
 }
