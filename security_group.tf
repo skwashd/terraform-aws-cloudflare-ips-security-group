20a66b40c3b8e3b7b0479b12a4296b4673b6745f
Dave Hall <skwashd@gmail.com>
2025-01-04 13:34:53 +1100

    Refactor to use aws_vpc_security_group_ingress_rule
    * Remove unnecessary ingress/egress blocks
    * Reformat for readability
    * Add tag for name

--- security_group.tf
+++ security_group.tf
@@ -3,33 +3,24 @@ resource "aws_security_group" "this" {
   description = "Cloudflare Source IPs for ${var.vpc_id}"
   vpc_id      = var.vpc_id
 
-  ingress {
-    description      = "Block ingress by default"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["127.0.0.1/32"]
-    ipv6_cidr_blocks = ["::1/128"]
-  }
-  egress {
-    description      = "Block egress by default"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["127.0.0.1/32"]
-    ipv6_cidr_blocks = ["::1/128"]
-  }
-
-  tags = var.tags
+  tags = merge(
+    {
+      Name = local.name
+    },
+    var.tags
+  )
 }
 
-resource "aws_security_group_rule" "ingress_tcp" {
-  for_each          = local.ports
-  type              = "ingress"
-  from_port         = each.key
-  to_port           = each.key
-  protocol          = "tcp"
-  cidr_blocks       = toset(data.cloudflare_ip_ranges.cloudflare.ipv4_cidr_blocks)
-  ipv6_cidr_blocks  = toset(data.cloudflare_ip_ranges.cloudflare.ipv6_cidr_blocks)
+resource "aws_vpc_security_group_ingress_rule" "ingress_tcp" {
+  for_each = local.ports
+
   security_group_id = aws_security_group.this.id
+  description       = "Allow ingress from Cloudflare on port ${each.key}"
+
+  cidr_ipv4 = toset(data.cloudflare_ip_ranges.cloudflare.ipv4_cidr_blocks)
+  cidr_ipv6 = toset(data.cloudflare_ip_ranges.cloudflare.ipv6_cidr_blocks)
+
+  ip_protocol = "tcp"
+  from_port   = each.key
+  to_port     = each.key
 }
